a999a76cc971c6d97b3d726147580591a65c2e10
Rajesh-Detroja <Rajesh-Detroja>
2020-07-09 16:13:11 +0300

    update!

--- getting_started/variables.R
+++ getting_started/variables.R
@@ -1,6 +1,7 @@
 # view the list of reserved words
 ?reserved
 
+# --------------------------------------------------
 
 # `TRUE` vs `True` as a variable name
 TRUE <- "Hello, World!"
@@ -9,79 +10,80 @@ print(TRUE)
 True <- "Rajesh Detroja"
 print(True)
 
+# --------------------------------------------------
 
 # Assignment using equal operator
-var_num = c(0,2.5,8,10,200)
+var_x = c(0,2.5,8,10,200)
 
 # Assignment using rightward operator
-var_logic <- TRUE
+var_y <- TRUE
 
 # Assignment using leftward operator
-c("Rajesh","Detroja") -> var_str
-
+c("Rajesh","Detroja") -> var_z
 
 # printing using `print()` function
-print(var_num)
-print(var_logic)
-print(var_str)
-
+print(var_x)
+print(var_y)
+print(var_z)
 
 # printing using `cat()` function
-cat("The values in `var_num` is:", var_num, "\n")
-cat("The values in `var_logic` is:", var_logic, "\n")
-cat("The values in `var_str` is:", var_str, "\n")
+cat("The values in `var_x` is:", var_x, "\n")
+cat("The values in `var_y` is:", var_y, "\n")
+cat("The values in `var_z` is:", var_z, "\n")
 
+# --------------------------------------------------
 
 # assigning data type to variables
-var_data_type <- "Rajesh Detroja"
-cat("The class of `var_data_type` is: ", class(var_data_type), "\n")
+var_x1 <- "Rajesh Detroja"
+cat("The class of `var_x1` is: ", class(var_x1), "\n")
 
-var_data_type <- 3.14
-cat("Now, the class of `var_data_type` is: ", class(var_data_type), "\n")
+var_x2 <- 3.14
+cat("Now, the class of `var_x2` is: ", class(var_x2), "\n")
 
-var_data_type <- 3L
-cat("Now, The class of `var_data_type` is: ", class(var_data_type), "\n")
+var_x3 <- 3L
+cat("Now, The class of `var_x3` is: ", class(var_x3), "\n")
 
-var_data_type <- FALSE
-cat("Now, The class of `var_data_type` is: ", class(var_data_type), "\n")
+var_x4 <- FALSE
+cat("Now, The class of `var_x4` is: ", class(var_x4), "\n")
 
-
+# --------------------------------------------------
 
 # make list of all the available variable in current environment
 print(ls())
 
+# --------------------------------------------------
 
 # make list of all the available variable in current environment using pattern
-print(ls(pattern = "type"))
+print(ls(pattern = "var_x"))
 
+# --------------------------------------------------
 
 # creating a hidden variable
-.var_hidden <- "Hello World!"
+.var_xy <- "Hello World!"
 
 # checking the class of a hidden variable
-print(class(.var_hidden))
+print(class(.var_xy))
 
 # listing all the variables
 print(ls())
-cat("\n")
 
 # listing all the variables including hidden variable
 print(ls(all.name = TRUE))
 
-
+# --------------------------------------------------
 
 # deleting the variable `.var_hidden`
-rm(.var_hidden)
+rm(.var_xy)
 
 # print deleted variable `.var_hidden`
-print(.var_hidden)
+print(.var_xy)
 
-
+# --------------------------------------------------
 
 # listing all the remaining variables
 print(ls())
 
-
+# --------------------------------------------------
 
 # deleting all the variables in current environment
 rm(list = ls())
