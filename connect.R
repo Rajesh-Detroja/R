5db1c531bc6cd3a4a271da39d976e2a81447cd9e
Rajesh-Detroja <rajesh.detroja.93@gmail.com>
2022-02-17 16:30:15 -0500

    uploaded

---
+++ connect.R
@@ -0,0 +1,17 @@
+## Connect Rstudio and Github
+
+
+## Install basic packages
+# sudo snap install curl
+# sudo apt install libcurl4-openssl-dev libssl-dev
+
+
+## Install library "usethis"
+install.packages("usethis")
+
+## Load library "usethis"
+library(usethis)
+
+## Connect
+use_git_config(user.name = "Rajesh-Detroja", user.email = "rajesh.detroja.93@gmail.com")
+
